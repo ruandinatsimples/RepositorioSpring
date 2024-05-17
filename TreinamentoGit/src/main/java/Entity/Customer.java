afa27176cb9a22047b1c241a7017908ba44641d7
Ruan Dinat <ruan.dinat@simples.software>
2024-05-17 13:52:45 -0300

    Ajuste classe entidade

--- TreinamentoGit/src/main/java/Entity/Customer.java
+++ TreinamentoGit/src/main/java/Entity/Customer.java
@@ -1,5 +1,37 @@
 package Entity;
 
 public class Customer {
+	
+	private Long id;
+	
+	private String name;
+	
+	private String document;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+	
+	
+	
 }
