ca85ad4b6c91e547108ca57408872597efd2caef
Ruan Dinat <ruan.dinat@simples.software>
2024-05-17 10:56:14 -0300

    Envio de projeto Spring para desenvolvimento

---
+++ src/main/java/com/treinamento/git/TreinamentoGitApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoGitApplication.class, args);
+	}
+
+}
